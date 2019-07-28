9919f71683bd4f3179d7602b8472886d91ad35ba
Q. Price <priceqp@gmail.com>
2019-07-27 19:01:27 -0500

    Create Navbar and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='Footer'>
+      <h5>Footer</h5>
+    </div>
+  );
+}
+
+export default Footer;
