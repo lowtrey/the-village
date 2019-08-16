d4fc799c59924907fe1d865f9260b805c1897ed4
Q. Price <priceqp@gmail.com>
2019-08-15 19:32:51 -0500

    Make Navbar a functional component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <div className='Navbar'>
-        <h1>Navbar</h1>
-      </div>
-    );
-  }
+function Navbar() {
+  return (
+    <div className='Navbar'>
+      <h1>Navbar</h1>
+    </div>
+  );
 }
 
 export default Navbar;
